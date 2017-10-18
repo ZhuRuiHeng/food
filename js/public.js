ea4745c0590189f07dc071b5b119f0bb8078c2c8
朱瑞恒 <18749830459@163.com>
2017-10-18 19:11:25 +0800

    10.23

--- js/public.js
+++ js/public.js
@@ -1,8 +1,8 @@
       var apiRoot = "https://shop.playonwechat.com";
       var operator_id = 67; //67
-      //var sign  = localStorage.getItem("sign");
+      var sign  = localStorage.getItem("sign");
 
-      var sign  = '524947f4f57689e5bc4d52b84c29ede9';
+      // var sign  = '524947f4f57689e5bc4d52b84c29ede9';
       // var nickname = localStorage.getItem(nickname);
       // var avatar   = localStorage.getItem(avatar);
       // if(!avatar){
@@ -16,9 +16,9 @@
       // $("#avatar").attr("src",avatar);
       // $("#nickname").html(nickname);
 
-      // if (!sign) {
-      //   window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc7b71ea532a9982c&redirect_uri=https%3a%2f%2fshop.playonwechat.com%2fapi%2fh5-auth&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
-      // }
+      if (!sign) {
+        window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc7b71ea532a9982c&redirect_uri=https%3a%2f%2fshop.playonwechat.com%2fapi%2fh5-auth&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
+      }
       //时间
       function time(unixtime, withTime) {
             if (!unixtime) {
